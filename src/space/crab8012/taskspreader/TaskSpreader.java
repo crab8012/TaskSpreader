95a63be5f9f243097d80bcbb57e06e60346bd3c0
Jordan Petersen <crab8011@gmail.com>
2018-04-19 23:10:14 -0600

    Initial Commit with the base version of the application.

---
+++ src/space/crab8012/taskspreader/TaskSpreader.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package space.crab8012.taskspreader;
+
+/**
+ *
+ * @author jorda
+ */
+public class TaskSpreader {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainFrame.main(args);
+    }
+    
+}
